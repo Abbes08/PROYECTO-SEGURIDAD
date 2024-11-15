6e4b5afd4fa68e3b1fb1d15c1411899b59578829
Ari0220 <131230710+Ari0220@users.noreply.github.com>
2024-11-15 12:10:42 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,28 @@
 const globals = require("globals");
 const pluginJs = require("@eslint/js");
 
-module.exports = [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-];
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  globals: {
+    ...globals.browser,
+    ...globals.node
+  },
+  extends: [
+    "eslint:recommended",
+    pluginJs.configs.recommended // Configuración recomendada del plugin JS
+  ],
+  rules: {
+    // Agrega aquí las reglas específicas que deseas activar
+    "no-unused-vars": "error",
+    "no-undef": "error",
+    "no-useless-escape": "error"
+  }
+};
