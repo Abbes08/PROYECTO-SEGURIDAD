9d1f6eeb5dce8b225885622c717b9bbdb49e915a
Abbes08 <gilespinoza99@gmail.com>
2024-11-18 15:19:41 -0600

    cambio enlist

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-// eslint.config.js
-const globals = require("globals");
-
-module.exports = [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.mocha // Añadido para reconocer `describe` e `it` en archivos de prueba
-      }
-    },
-    rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
-      "no-useless-escape": "error"
-    }
-  }
-];
-=======
 // .eslintrc.js
 const globals = require("globals");
 
@@ -49,4 +25,3 @@ module.exports = {
     "no-useless-escape": "error"
   }
 };
->>>>>>> 34322a83f79f8caacbd8ca538a0400c52b8229d0
